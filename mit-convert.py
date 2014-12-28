5d5c9ef9c87a7e4c6623ed8e1f86f3816777fca7
Thomas Bachman <tbachman@yahoo.com>
2014-12-27 19:46:55 -0800

    Working version of parser.
    Signed-off-by: Thomas Bachman <tbachman@yahoo.com>

--- mit-convert.py
+++ mit-convert.py
@@ -1,3 +1,10 @@
+#
+# This isn't the prettiest python code, but it achieves
+# what I need -- convert the metadata.cpp file output by
+# the Genie code generator into a java file that creates
+# the equivalent schema
+#
+#
 import re
 
 METADATA_FILE='metadata.cpp'
@@ -8,6 +15,7 @@ OPEN_PAREN="("
 CLOSE_PAREN=")"
 WS=' '
 NL='\n'
+LIST_SEP=','
 
 # This is the regular expression that can be used to
 # extract a string of items from the PropertyInfo object
@@ -18,29 +26,68 @@ PROPERTIES_RE='\(PropertyInfo\(([^\)]*).*'
 CLASS_RE='ClassInfo\((.*)'
 
 class PropertyInfo:
+    def extract_tokens(self, enum_token):
+        '''Get the EnumInfo tokens'''
+        count = 0
+        startpos = 0
+        while 1 == 1:
+            a,startpos,b,endpos=match_parenthesis(enum_token,count,startpos)
+            if startpos == -1:
+                break
+            startpos += 1
+            end_token = endpos + 1
+            a,startpos,b,endpos=match_parenthesis(enum_token,count,startpos)
+            startpos += 1
+            self.token_array.append(enum_token[a][startpos:endpos])
+            startpos = end_token
     def __init__(self, propstring):
         '''Constructor passes in a string of properties.
            We have to figure out how many there are, strip
            out any whitespace, and put them into their
            appropriate fields'''
-        proplist=[prop.strip() for prop in propstring.split(',')]
+        self.pid = ''
+        self.cid = ''
+        self.name = ''
+        self.type = ''
+        self.cardinality = ''
+        self.enum_name = ''
+        self.token_array = []
+        proplist=[prop.strip() for prop in propstring.split(LIST_SEP)]
         if len(proplist) == 4:
             self.pid, self.name, self.type, self.cardinality = proplist
         elif len(proplist) == 5:
             self.pid, self.name, self.type, self.cid, self.cardinality = proplist
         else:
+            self.pid, self.name, self.type, self.cardinality = proplist[0:4]
             # This case handles EnumInfo's. We can't split it because
             # it splits up the EnumInfo properties. Instead, we have
             # to parse it, knowing what an EnumInfo format looks like
+            pos = 0
+            count = 0
+            tmparray=[]
+            tmparray.append(propstring)
+            a,startpos,b,endpos=match_parenthesis(tmparray,count, pos)
+            startpos += 1
+            enum_token = propstring[startpos:endpos]
+            # EnumInfo name is first item in remaining comma-sep list
+            self.enum_name=enum_token.split(LIST_SEP)[0]
+            enum_array=[]
+            enum_array.append(enum_token)
+            # Now get the enum tokens
+            self.extract_tokens(enum_array)
 
 
 class ClassInfo:
-    self.properties=[]
     def __init__(self, class_string, prop_array):
-
-        field_list=[field.strip() for field in class_string.split(',')]
-        self.cid, self.ctype, self.name, self.owner = field_list[0:]
-
+        '''Initialize the class, using the class info string
+           and the list of properties.'''
+        self.cid = ''
+        self.ctype = ''
+        self.name = ''
+        self.owner = ''
+        self.properties = []
+        field_list=[field.strip() for field in class_string.split(LIST_SEP)]
+        self.cid, self.ctype, self.name, self.owner = field_list[0:-1]
         for prop in prop_array:
             self.properties.append(PropertyInfo(prop))
 
@@ -71,7 +118,7 @@ def match_parenthesis(ll, count, pos):
     pos=ll[count].find(OPEN_PAREN, pos)
     # If no open parenthesis in first line, quit
     if pos == -1:
-        return count
+        return -1,-1,-1,-1
     paren_list.append((count,pos))
     pos = pos + 1
     startcnt, startpos, endcnt, endpos = 0,0,0,0
@@ -86,7 +133,7 @@ def match_parenthesis(ll, count, pos):
             continue
         # either both positive, or one is -1
         if popos < pcpos:
-            if popos > 0:
+            if popos >= 0:
                 paren_list.append((count,popos))
                 pos=popos+1
             else:
@@ -94,7 +141,7 @@ def match_parenthesis(ll, count, pos):
                 endcnt, endpos = count, pcpos
                 pos=pcpos+1
         elif pcpos < popos:
-            if pcpos > 0:
+            if pcpos >= 0:
                 startcnt, startpos = paren_list.pop()
                 endcnt, endpos = count, pcpos
                 pos=pcpos+1
@@ -154,27 +201,16 @@ while 1 == 1:
     lines=find_token(lines, CI_OPEN)
     if lines == None:
         break
-    
     # Get the class fields string
     class_string=re.match(CLASS_RE, lines[0].strip()).group(1)
-
     # Start with a clean prop array
     prop_array=[]
-    classlist=[prop.strip() for prop in class_string.split(',')]
-
-    # Get the class properties
-
+    classlist=[prop.strip() for prop in class_string.split(LIST_SEP)]
     # we know the next line is always a "list_of", so
     # skip it and get all the properties
     lines=get_properties(lines[2:], prop_array)
-
     # See if there are any keys associated with this class.
     # If there are, set the "is_key" field in the associated
     # property.
-    if re.match(LIST_OF_RE, lines[1:]):
-        
-
-    # We have a new class -- create the object
     c=ClassInfo(class_string, prop_array)
-
     class_list.append(c)
